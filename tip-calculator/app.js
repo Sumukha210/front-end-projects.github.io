06e9228d4976a71d9e02dfc2a1ad1e588094193f
sumukha <sumukhakb210@gmail.com>
2020-07-11 22:25:54 +0530

    tip calculator design

---
+++ tip-calculator/app.js
@@ -0,0 +1,6 @@
+const tip_input = document.getElementById("tip_input");
+const no_people_input = document.getElementById("no_people_input");
+const tip_per_dom = document.getElementById("tip_percent");
+const tot_amount = document.getElementById("tot_amount");
+const no_people_parentDiv = document.getElementById("no_people_div");
+const tip_parent_div = document.getElementById("tip_div");
